a454fedd0e89601080af10d9e085cefe8fb20824
Rafsan12 <rafsan.web.2022@gmail.com>
2025-04-16 10:53:31 +0600

    metatag add

--- app/layout.js
+++ app/layout.js
@@ -5,11 +5,10 @@ import { getPageMap } from "nextra/page-map";
 import "./globals.css";
 
 export const metadata = {
-  // Define your metadata here
-  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+  title: "Bangla Tech Notes",
+  description: "Notes in Bangla covering various modern web technologies.",
 };
 
-// const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>;
 const navbar = (
   <Navbar
     logo={
@@ -19,35 +18,29 @@ const navbar = (
       </>
     }
     projectLink="https://github.com/Rafsan12/nextra-blog"
-
-    // ... Your additional navbar options
   />
 );
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>;
+
+const footer = (
+  <div className=" py-4  text-gray-500">
+    <Footer>
+      <h1 className="text-center">MIT {new Date().getFullYear()} © Nextra.</h1>
+    </Footer>
+  </div>
+);
 
 export default async function RootLayout({ children }) {
   return (
-    <html
-      // Not required, but good for SEO
-      lang="en"
-      // Required to be set
-      dir="ltr"
-      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
-      suppressHydrationWarning
-    >
-      <Head
-      // ... Your additional head options
-      >
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+    <html lang="en" dir="ltr" suppressHydrationWarning>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body>
+      <body suppressHydrationWarning>
         <Layout
-          // banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
           footer={footer}
-          // ... Your additional layout options
         >
           {children}
         </Layout>
